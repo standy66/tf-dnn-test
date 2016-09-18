009d9e1dc6d087c401d65d05e8596d14f2c70340
Andrew Stepanov <karn9050@gmail.com>
2016-09-19 01:52:12 +0300

    Add residual connections to neural net

--- deep_net.py
+++ deep_net.py
@@ -7,7 +7,8 @@ from tensorflow.examples.tutorials.mnist import input_data
 mnist = input_data.read_data_sets('Mnist_data', one_hot=True)
 
 num_input = 784
-hidden_size = 500
+hidden_size = 784
+hidden_size_2 = 200
 num_classes = 10
 num_epochs = 30
 batch_size = 100
@@ -18,20 +19,36 @@ y = tf.placeholder('float')
 
 def model(x):
     """Define a model."""
-    hl1 = {"weights": tf.Variable(tf.random_normal([num_input, hidden_size])),
-           "biases": tf.Variable(tf.random_normal([hidden_size]))}
+    hl1 = {"weights": tf.Variable(tf.truncated_normal([num_input, hidden_size], stddev=0.5)),
+           "biases": tf.Variable(tf.truncated_normal([hidden_size], stddev=0.5))}
 
-    hl2 = {"weights": tf.Variable(tf.random_normal([hidden_size, hidden_size])),
-           "biases": tf.Variable(tf.random_normal([hidden_size]))}
+    hl2 = {"weights": tf.Variable(tf.truncated_normal([hidden_size, hidden_size], stddev=0.5)),
+           "biases": tf.Variable(tf.truncated_normal([hidden_size], stddev=0.5))}
 
-    output = {"weights": tf.Variable(tf.random_normal([hidden_size, num_classes])),
-              "biases": tf.Variable(tf.random_normal([num_classes]))}
+    # l2l4 = tf.Variable(tf.truncated_normal([hidden_size, hidden_size_2], stddev=0.5))
+
+    # hl3 = {"weights": tf.Variable(tf.truncated_normal([hidden_size, hidden_size_2],
+    #                                                   stddev=0.5)),
+    #       "biases": tf.Variable(tf.truncated_normal([hidden_size_2], stddev=0.5))}
+
+    # hl4 = {"weights": tf.Variable(tf.truncated_normal([hidden_size_2, hidden_size_2],
+    #                                                   stddev=0.5)),
+    #       "biases": tf.Variable(tf.truncated_normal([hidden_size_2], stddev=0.5))}
+
+    output = {"weights": tf.Variable(tf.truncated_normal([hidden_size, num_classes], stddev=0.5)),
+              "biases": tf.Variable(tf.truncated_normal([num_classes], stddev=0.5))}
 
     l1 = tf.matmul(x, hl1["weights"]) + hl1["biases"]
     l1 = tf.nn.relu(l1)
 
     l2 = tf.matmul(l1, hl2["weights"]) + hl2["biases"]
-    l2 = tf.nn.relu(l2)
+    l2 = tf.nn.relu(l2 + x)
+
+    # l3 = tf.matmul(l2, hl3["weights"]) + hl3["biases"]
+    # l3 = tf.nn.relu(l3)
+
+    # l4 = tf.matmul(l3, hl4["weights"]) + hl4["biases"]
+    # l4 = tf.nn.relu(l4 + tf.matmul(l2, l2l4))
 
     return tf.matmul(l2, output["weights"]) + output["biases"]
 
